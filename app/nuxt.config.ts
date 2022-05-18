ae1048ea73da6be01739e0be06faa54330a04041
onishi.kohei <kohei.o2222@gmail.com>
2022-05-18 23:21:06 +0900

    actions追加

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,5 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  router: {
+    base: '/portfolio/'
+  },
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/project_name/favicon.ico' }
+  ]
 })
