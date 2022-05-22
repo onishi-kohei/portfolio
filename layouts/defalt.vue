d486fdcdb0452bc313d8037b8960913d2abbb5eb
onishi.kohei <kohei.o2222@gmail.com>
2022-05-22 12:29:09 +0900

    layout追加

---
+++ layouts/defalt.vue
@@ -0,0 +1,8 @@
+
+<template>
+  <slot />
+</template>
+<script lang="ts" setup>
+</script>
+<style lang="scss">
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>index</div>
+</template>
+<script setup>
+definePageMeta({
+  layout: 'defalt'
+})
+</script>
