c53ea52bdd91d30310c4f2d64fb8d9654251d9fe
onishi.kohei <kohei.o2222@gmail.com>
2022-05-22 21:57:09 +0900

    lint設定変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,13 +14,11 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     sourceType: 'module'
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint'
-  ],
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['error'],
-    'vue/no-multiple-template-root': 'off'
+    'vue/no-multiple-template-root': 'off',
+    'vue/multi-word-component-names': 'off'
   }
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,9 @@
 import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   router: {
     base: '/portfolio/'
   },
-  link: [
-    { rel: 'icon', type: 'image/x-icon', href: '/project_name/favicon.ico' }
-  ]
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/project_name/favicon.ico' }],
+  css: ['@/assets/css/reset.css']
 })
